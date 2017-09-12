e0d5b7c0942baab82438dfbee64786dcb5cc5802
Deepak <deepakvj92@gmail.com>
2017-09-12 22:38:00 +0530

    post detail page

---
+++ src/components/post_detail.vue
@@ -0,0 +1,42 @@
+<template>
+    <span>
+      <v-parallax src="https://unsplash.it/g/1280/700?random" height="400" class="full" ></v-parallax>
+      <v-container class="content">
+        <v-layout row wrap>
+          <v-flex sm12>
+            <br>
+             <h4 class="center primary--text " >Lorem ipsum sodor Hodor hold the door!</h4> 
+             <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis esse quo, necessitatibus tempora veritatis aliquam voluptatum reprehenderit nisi non ad incidunt, totam, minima velit. Facere in rerum ullam maiores iusto?</p>
+             <p >Deleniti delectus ex facere libero quod harum possimus, illo reiciendis corporis id aspernatur unde et, eos esse optio neque aperiam molestiae voluptates aut nobis, sunt doloremque! Ex cupiditate, quasi ad.</p>
+             <p >Dolor, totam et rem, vero, ratione tempore consequatur beatae nihil quasi alias esse, veritatis veniam dicta ipsam fugit. Voluptatum numquam eaque, enim vero modi iure nesciunt odit facilis officia illo!</p>
+             <p >Deserunt incidunt voluptatem facilis laboriosam quasi eos illo laudantium sed consectetur beatae, reprehenderit doloribus vitae nemo iure, similique voluptatum itaque ipsa nulla tempore fugiat magni excepturi mollitia. Nobis, dolores velit.</p>
+             <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, voluptatibus quae doloribus libero voluptas repellendus optio labore sapiente quos dolore consequatur omnis cum, nihil, dolor explicabo sunt aut? Quis, nostrum.</p>
+             <p >Nesciunt id, dolorem eos non, voluptatum, illum nihil adipisci in, harum doloribus ratione cupiditate fugiat! Eveniet obcaecati est laborum molestias aperiam neque facere explicabo nesciunt odit, quo, corporis iusto cupiditate!</p>
+             <p >Vitae cumque repellat esse molestias. Ab quidem enim officia suscipit. Similique quas, non perferendis deserunt asperiores nam eveniet consectetur dolor iure ullam ut quia temporibus doloremque blanditiis harum. Qui, sapiente.</p>
+             <p >Ad, quia quidem facere. Iusto et asperiores eum voluptatum, itaque voluptatem aliquam quas illum accusamus sunt adipisci delectus laboriosam nobis quos rerum minima labore ullam. Nulla asperiores voluptatem quidem pariatur!</p>
+             <p >Natus ducimus, temporibus itaque voluptatum, assumenda reiciendis. Asperiores praesentium ex unde labore maxime delectus laudantium debitis nam eligendi corrupti tempora autem quasi accusantium dolorum quos recusandae, cumque. Aliquid, similique, dolores.</p>
+             <p >Nihil perferendis maxime quos porro eveniet quae sint, nesciunt mollitia sunt alias animi molestiae ad esse provident voluptatibus minus error temporibus dolorum cumque in rerum illo! Iste harum possimus placeat.</p>
+             <p >Consectetur nisi odit accusantium provident ratione, dolorem nulla doloribus neque cum tempora, voluptatibus, facilis alias quod eaque fugiat, qui ducimus laboriosam! Provident voluptatibus neque nostrum inventore ex numquam, similique molestiae.</p>
+          </v-flex>  
+        </v-layout>  
+      </v-container> 
+    </span>  
+</template>
+<script>
+export default {
+  mounted () {
+  }
+}
+</script>
+<style scoped>
+.container {
+  width: 100% !important;
+  padding: 0px !important;
+}
+.center {
+  text-align:center;
+}
+.full {
+  width: 100%;
+}
+</style>
