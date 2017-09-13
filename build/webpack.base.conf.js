70d316373dbfeea597177a5e3d5dfb2026fc68da
Deepak <deepakvj92@gmail.com>
2017-09-13 18:01:39 +0530

    fixes for masonry aliases

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,9 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'masonry': 'masonry-layout',
+      'isotope': 'isotope-layout'
     }
   },
   module: {
