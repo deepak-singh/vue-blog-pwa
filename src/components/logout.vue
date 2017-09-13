db0772a12f768c67ec61a46ca67d830d45110a16
Deepak <deepakvj92@gmail.com>
2017-09-13 18:02:49 +0530

    logout component

---
+++ src/components/logout.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    <v-btn class="secondary" @click="logout">Logout</v-btn>
+  </span>
+</template>
+<script>
+import State from '@/store'
+export default {
+  data () {
+    return {
+      state: State
+    }
+  },
+  methods: {
+    logout () {
+      window.localStorage.removeItem('api_key')
+      this.state.username = undefined
+      this.state.last_login = undefined
+    }
+  }
+}
+</script>
