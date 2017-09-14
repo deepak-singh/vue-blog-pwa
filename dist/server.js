486c640472239bb83cd8704258fc4fa1da85feba
Deepak <deepakvj92@gmail.com>
2017-09-15 01:35:45 +0530

    express fallback fro ht=istory mode

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
+var history = require('connect-history-api-fallback');
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(history());
 app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port);
