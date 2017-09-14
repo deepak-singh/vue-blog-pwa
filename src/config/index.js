473cb48ed94a24c3b3cc1e1da5625a1b961957d8
Deepak <deepakvj92@gmail.com>
2017-09-14 14:59:31 +0530

    configuring axios separately

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,14 @@
-export default {
-  'api': 'http://localhost:8000/api/v1/',
-  'domain': 'http://localhost:8000'
+import axios from 'axios'
+
+let HTTP = axios.create({
+  baseURL: 'http://localhost:8000/api/v1/',
+  timeout: 1000,
+  Authorization: undefined
+})
+
+if (window.localStorage.getItem('api_key_header')) {
+  console.log('setting header from local storage')
+  HTTP.defaults.headers.common['Authorization'] = window.localStorage.getItem('api_key_header')
 }
+
+export default HTTP
