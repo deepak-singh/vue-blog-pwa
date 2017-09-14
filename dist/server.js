e911afa7eefb25ee21cca2216a2e83371088a7f6
Deepak <deepakvj92@gmail.com>
2017-09-15 01:52:17 +0530

    history fallback

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,14 @@
-var history = require('connect-history-api-fallback');
 var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+const app = express();
 
-app = express();
-app.use(history());
-app.use(express.static(__dirname));
+const staticFileMiddleware = express.static(__dirname);
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
