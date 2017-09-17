00913847314527eae715313a08e79934fa099958
Deepak <deepakvj92@gmail.com>
2017-09-17 13:58:05 +0530

    heroku ssl plugin

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,12 @@
+var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
+var app = express();
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+
 app.use(serveStatic(__dirname));
+app.use(sslRedirect());
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
