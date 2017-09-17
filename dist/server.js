90827ab8af6976a74c034cfa21f1ecb9b3060aec
Deepak <deepakvj92@gmail.com>
2017-09-17 14:18:29 +0530

    ssl fixes

--- dist/server.js
+++ dist/server.js
@@ -1,19 +1,10 @@
-// var sslRedirect = require('heroku-ssl-redirect');
-
+var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var app = express();
 var path = require('path');
 var serveStatic = require('serve-static');
-
+app.use(sslRedirect());
 app.use(serveStatic(__dirname));
-
-app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto']!='https')
-    res.redirect('https://mypreferreddomain.com'+req.url)
-  else
-    next() /* Continue to other routes if we're not redirecting */
-})
-
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
