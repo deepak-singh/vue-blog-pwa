b8a5547da7d9384716c77ed9f3926d2799644a14
Deepak <deepakvj92@gmail.com>
2017-09-17 14:12:22 +0530

    plugin removed and code updated for https

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,18 @@
-var sslRedirect = require('heroku-ssl-redirect');
+// var sslRedirect = require('heroku-ssl-redirect');
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var serveStatic = require('serve-static');
 
 app.use(serveStatic(__dirname));
-app.use(sslRedirect());
+
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    res.redirect('https://mypreferreddomain.com'+req.url)
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port);
