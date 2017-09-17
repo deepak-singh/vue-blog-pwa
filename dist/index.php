2613aec254b343e4d502c385fcad5b5252d2d5d4
Deepak <deepakvj92@gmail.com>
2017-09-17 19:37:03 +0530

    fixes

--- dist/index.php
+++ dist/index.php
@@ -2,14 +2,15 @@
 
 <?php 
 
+
+header( 'Location: /index.html' ) ;  
+
 if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
     $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $redirect);
-    exit();
 }
 
-header( 'Location: /index.html' ) ;  
 
 ?>
 
